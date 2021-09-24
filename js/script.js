92b9c4140d93f544d3c18f99d8f9c04963456f01
Sophia L <sophitole@gmail.com>
2021-09-24 17:54:49 -0600

    Added scrolling platform

--- js/script.js
+++ js/script.js
@@ -1,13 +1,57 @@
+// Background
 var bg = new Image();
+// Platform
+var pf = new Image();
+bg.src = "assets/kirbydreamland.jpeg";
+pf.src = "assets/grasstile.png"
+var clearX;
+var clearY;
+var speed = 0.7; // lower is faster
+var scale = 1.05;
+var dx = -0.75;
+var ctx;
 canvas.width = 800;
 canvas.height = 451;
-bg.src = "assets/kirbydreamland.jpeg";
+var x = 0;
+var y = canvas.height - 100;
+
+
 bg.onload = function() {
     scaleToFit(this);
 }
 
+pf.onload = function() {
+    imgW = pf.width * scale;
+    imgH = pf.height * scale;
+
+    // get canvas context
+    ctx = document.getElementById('canvas').getContext('2d');
+
+    // set refresh rate
+    return setInterval(draw, speed);
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // clear the canvas
+    scaleToFit(bg);
+
+    // reset, start from beginning
+    if (x < -imgW) {
+        x = 0;
+    }
+    // draw additional image
+    if (x < canvas.width - imgW) {
+        ctx.drawImage(pf, x + imgW, y, imgW, imgH);
+    }
+
+    // draw image
+    ctx.drawImage(pf, x, y, imgW, imgH);
+    // amount to move
+    x += dx;
+}
+
 function scaleToFit(img) {
-    var ctx = document.getElementById('canvas').getContext('2d');
+    ctx = document.getElementById('canvas').getContext('2d');
     // get the scale
     var scale = Math.min(canvas.width / img.width, canvas.height / img.height);
     // get the top left position of the image
